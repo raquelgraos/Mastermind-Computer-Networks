346c6d1134a226b569a674d2c64d98dc6f06efb2
Guilherme Pereira <pereira.gui887@gmail.com>
2024-11-21 18:18:03 +0000

    Loopback ip

--- client/player_main.h
+++ client/player_main.h
@@ -1,8 +1,8 @@
 #ifndef _PLAYER_MAIN_H_
 #define _PLAYER_MAIN_H_
 
-#define DEFAULT_PORT "58011"
-#define DEFAULT_GSIP "193.136.138.142"
+#define DEFAULT_PORT "58011" //TODO
+#define DEFAULT_GSIP "127.0.0.1"
 
 void print_usage(const char *prog_name);
 int main(int argc, char *argv[]);
