f418c7e06e0e5d3e48c2b53879a775e3624fbc85
raquelgraos <raquelgrodrigues04@gmail.com>
2024-11-25 12:19:58 +0000

    added tejo port

--- client/player_main.h
+++ client/player_main.h
@@ -1,7 +1,7 @@
 #ifndef _PLAYER_MAIN_H_
 #define _PLAYER_MAIN_H_
 
-#define DEFAULT_PORT "58060" //TODO
+#define DEFAULT_PORT "58060" // "58011" //tejo
 #define DEFAULT_GSIP "127.0.0.1" //"193.136.138.142" //tejo
 
 void print_usage(const char *prog_name);
